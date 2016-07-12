5f1f9280466c28e55e7016825174b1fd5811b66a
ZapAnton <wc3ft2@gmail.com>
2016-07-12 17:41:00 +0300

    Added a number guessing algorithm

--- Anton/guess_number.py
+++ Anton/guess_number.py
@@ -1,6 +1,13 @@
 import random
 
-var = input("Enter your number: ")
+var = int(input("Enter your number: "))
 print("Your number is: ", var)
 
-print(random.randrange(0, int(var)))
+times_to_guess = 0
+guessed_number = -1
+
+while guessed_number != var:
+    guessed_number = random.randrange(0, var + 1)
+    times_to_guess += 1
+
+print("It took", times_to_guess, "guesses to find your number")
