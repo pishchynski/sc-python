466392af01bbeafe4bdcd9edfecd9dc040754df4
ZapAnton <wc3ft2@gmail.com>
2016-07-15 17:59:02 +0300

    Made approximation function

--- Anton/training.py
+++ Anton/training.py
@@ -1,4 +1,21 @@
 suffixes = {1000: ['KB', 'MB', 'GB', 'TB'],
             1024: ['KiB', 'MiB', 'GiB', 'TiB']}
 
-print(suffixes[1000])
+
+def approximate_size(size, a_kilobyte_is_1024_byte=True):
+    if size < 0:
+        raise ValueError("Number must be non-negative!")
+
+    multiple = 1024 if a_kilobyte_is_1024_byte else 1000
+
+    for suffix in suffixes[multiple]:
+        size /= multiple
+
+        if size < multiple:
+            return "{0:.1f} {1}".format(size, suffix)
+
+    raise ValueError("Number is too large")
+
+
+print(approximate_size(1000000000000, False))
+print(approximate_size(1000000000000))
