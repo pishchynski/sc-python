bfc9a0f8d13a8aa6695cc40cd9471c3c679b9160
ZapAnton <wc3ft2@gmail.com>
2016-07-19 12:40:20 +0300

    List indexing test

--- Anton/examples_collections.py
+++ Anton/examples_collections.py
@@ -1,3 +1,24 @@
 nums = [1, 2, 3]
 chars = list("Python")
 pows = [2 ** i for i in range(20) if i % 4 == 2]
+
+print(pows)
+print(pows[1])
+print(pows[-3])
+print(pows[2:5])
+print(pows[:3])
+print(pows[3:])
+print("Len=", len(pows))
+
+test_list = [10, 20, 30]
+test_sub_list = test_list[1:2]
+
+print(test_list)
+print(test_sub_list)
+
+test_sub_list[0] = 999
+
+print(test_list)
+print(test_sub_list)
+
+print([1, 2, 3][::-1])
