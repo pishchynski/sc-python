fa2691509fe0ae66e078c1e995b41a3a3f19f336
ZapAnton <wc3ft2@gmail.com>
2016-07-19 12:12:34 +0300

    Testing lambdas

--- Anton/examples.py
+++ Anton/examples.py
@@ -22,15 +22,6 @@ print(not 2)
 
 print("IS:")
 
-z = 3
-x = z
-c = 4
-
-print(z is x)
-print(z is z)
-print(x is z)
-print(c is z)
-
 print(1 if 8 else 123)
 
 # result = eval(input("Enter smth: "))
@@ -49,14 +40,6 @@ while a > 34:
         break
 else:
     print("Already deced")
-
-n = 100
-s = 0
-
-for i in range(n + 1):
-    s += i
-
-print(s)
 
 for c in "Python":
     print(c)
@@ -90,4 +73,56 @@ def my_sum(*numbers):
         summ += number
     return summ
 
+
 print(my_sum(1, 2, 99))
+
+
+def first_func(txt):
+    print("First func:", txt)
+
+
+my_func = first_func
+
+my_func("Testing")
+
+
+def solve_eq(f, x0, n):
+    res = x0
+    for k in range(1, n + 1):
+        res = f(res)
+    return res
+
+
+def eq_1(x1):
+    return x1 ** 2 - 5
+
+
+def eq_2(x2):
+    return (6 * x2 - 5) ** 0.5
+
+
+x = solve_eq(eq_1, 0, 10)
+print("First equation:", x)
+x = solve_eq(eq_2, 4, 10)
+print("Second equation:", x)
+
+
+def fib(n):
+    if (n == 1) or (n == 2):
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+for i in range(1, 16):
+    print("Fib", i, "=", fib(i), end=" ")
+
+print()
+
+
+def find_value(f, x):
+    print("x =", x, " f(x) =", f(x))
+
+
+find_value(lambda x=9: 1 / (1 + x ** 2), 2.0)
+
