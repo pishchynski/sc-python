80c1cc59020d6560f218a7a3fa45baeca7c30453
ZapAnton <wc3ft2@gmail.com>
2016-07-21 14:27:05 +0300

    Added c++ alarm_executer

---
+++ Anton/youtube_alarm/alarm_executer/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_ARTIFACT_NAME_Debug=alarm_executer
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/alarm_executer
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_Debug=alarmexecuter.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/alarmexecuter.tar
+# Release configuration
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
+CND_ARTIFACT_NAME_Release=alarm_executer
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/alarm_executer
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
+CND_PACKAGE_NAME_Release=alarmexecuter.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/alarmexecuter.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
