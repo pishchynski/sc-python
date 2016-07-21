2ab68902bc1d8cd1f830ec6965c7b90f4b6fda58
ZapAnton <wc3ft2@gmail.com>
2016-07-21 11:08:53 +0300

    Class field testing

--- Anton/examples_classes.py
+++ Anton/examples_classes.py
@@ -1,6 +1,16 @@
 class TestClass:
-    def __init__(self):
+    name = "Class TestClass"
+
+    def set(self, n):
+        self.nickname = n
+
+    def get(self):
+        print("Nickname =", self.nickname)
+
+    def __init__(self, n):
+        self.set(n)
         print("INITIALIZATION")
+        self.get()
 
     def __del__(self):
         print("DELETING")
@@ -10,12 +20,23 @@ def outer_method():
     print("Method is now in the class")
 
 
-test = TestClass()
-test.prt = outer_method
+test = TestClass("Green")
+print("Green NAME =", test.name)
 
-test.prt()
+red = TestClass("Red")
+print("Red NAME =", red.name)
+
+TestClass.name = "CHANGED NAME"
+
+print("Red NAME =", red.name)
+print("Green NAME =", test.name)
+
+red.name = "CHANGED RED NAME"
+
+print("Red NAME =", red.name)
+print("Green NAME =", test.name)
 
 del test
+del red
 
-print("AFTER")
-
+print("Program END")
