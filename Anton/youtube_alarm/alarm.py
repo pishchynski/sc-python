ce2f06224efd7a4e6b4ce3e2f06c4e9cdcfd5e06
ZapAnton <wc3ft2@gmail.com>
2016-07-21 13:57:41 +0300

    File reading is now in a method. Choosing random link

--- Anton/youtube_alarm/alarm.py
+++ Anton/youtube_alarm/alarm.py
@@ -1,3 +1,20 @@
-links_file = open("links.txt", "r")
-links = [link.strip("\n") for link in links_file]
-links_file.close()
+from random import randint
+
+
+def get_links(filename: "Name of the file with links") -> list:
+    """Return a list where each elements is a link string"""
+    links_file = open(filename, "r")
+
+    links = [link.strip("\n") for link in links_file]
+
+    links_file.close()
+    return links
+
+
+links = get_links("links.txt")
+
+random_link_index = randint(0, len(links) - 1)
+random_link = links[random_link_index]
+
+print(links)
+print(random_link)
