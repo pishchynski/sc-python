2dd1f8ef7178d3546f2c6279b4544e780b254d20
ZapAnton <wc3ft2@gmail.com>
2016-07-25 17:28:44 +0300

    Numpy arrays examples

---
+++ Anton/ml/numpy_learn.py
@@ -0,0 +1,27 @@
+import numpy as np
+a = np.array([1, 2, 3])
+print(a)
+
+b = np.array([[1.5, 2, 3], [4, 5, 6]], dtype=np.complex)
+print(b)
+
+print()
+
+print(np.zeros((3, 5)))
+
+print()
+
+print(np.ones((2, 2, 2)))
+
+print()
+
+print(np.eye(5))
+
+print()
+
+print(np.arange(0, 1, 0.1))
+
+print()
+
+print(np.linspace(1, 5, 9))
+
