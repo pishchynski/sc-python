fe9b6d63d5a16e76ed6894098ebd1d5fbecd0ff7
pishchynski <pavel.pishchynski@softclub.by>
2016-08-01 14:38:13 +0300

    donate_blood initial commit

---
+++ Machine_learn/donate_blood/Pasha/Blood/script/blood.py
@@ -0,0 +1,34 @@
+import numpy as np
+with open('../data/train_data.csv', mode='r') as f:
+    dataset = np.loadtxt(f, delimiter=',')
+
+ids = dataset[1:,0]
+features = dataset[1:, 1:5]
+targets = dataset[1:, 5]
+
+# normalizing data
+from sklearn import preprocessing
+n_features = preprocessing.normalize(features)
+
+# feature selection
+
+from sklearn.ensemble import ExtraTreesClassifier
+model = ExtraTreesClassifier()
+model.fit(n_features, targets)
+
+print(model.feature_importances_)
+
+# algorythm
+from sklearn import metrics
+from sklearn.svm import SVC
+model = SVC()
+model.fit(n_features, targets)
+print(model)
+
+# predict
+expected = targets
+predicted = model.predict(n_features)
+
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
