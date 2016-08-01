d4f304bc07342c2653bca90142165bcb6eb412c7
ZapAnton <wc3ft2@gmail.com>
2016-08-01 16:03:20 +0300

    NumPy refresher

--- Machine_learn/donate_blood/Anton/donate_blood.py
+++ Machine_learn/donate_blood/Anton/donate_blood.py
@@ -1,2 +1,11 @@
-import theano.tensor as T
+import numpy as np
+
+
+print(np.asarray([[1, 2], [3, 4], [5, 6]]))
+print()
+print(np.asarray([[1, 2], [3, 4], [5, 6]]).shape)
+print()
+print(np.asarray([[1, 2], [3, 4], [5, 6]])[2, 0])
+print()
+print(np.asarray([[1, 2], [3, 4], [5, 6]]) * 2)
 
