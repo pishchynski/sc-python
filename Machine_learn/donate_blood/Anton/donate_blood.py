0921b794e0d2c2625e6f2fe39a3f454a4045dbcb
ZapAnton <wc3ft2@gmail.com>
2016-08-01 17:34:23 +0300

    Theano test - regression

--- Machine_learn/donate_blood/Anton/donate_blood.py
+++ Machine_learn/donate_blood/Anton/donate_blood.py
@@ -57,3 +57,51 @@ print(state.get_value())
 decrementor(2)
 print(state.get_value())
 
+
+print()
+print()
+print()
+
+rng = np.random
+
+N = 400
+feats = 784
+
+D = (rng.randn(N, feats), rng.randint(size=N, low=0, high=2))
+training_steps = 10000
+
+x = T.matrix('x')
+y = T.matrix('y')
+
+w = shared(rng.randn(feats), name='w')
+
+b = shared(0., name='b')
+
+print("Initial model:")
+print(w.get_value())
+print(b.get_value())
+
+p_1 = 1 / (1 + T.exp(-T.dot(x,w) - b))
+prediction = p_1 > 0.5
+xent = -y * T.log(p_1) - (1 - y) * T.log(1 - p_1)
+cost = xent.mean() + 0.01 * (w ** 2).sum()
+
+gw, gb = T.grad(cost, [w, b])
+
+train = function(
+          inputs=[x,y],
+          outputs=[prediction, xent],
+          updates=((w, w - 0.1 * gw), (b, b - 0.1 * gb)))
+
+predict = function(inputs=[x], outputs=prediction)
+
+for i in range(training_steps):
+    pred, err = train(D[0], D[1])
+
+print("Final model:")
+print(w.get_value())
+print(b.get_value())
+print("target values for D:")
+print(D[1])
+print("prediction on D:")
+print(predict(D[0]))
