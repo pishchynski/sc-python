7807a9103a9d1d53060ff250b4509b6d84e7c4ca
pishchynski <pavelalexbel@outlook.com>
2016-08-05 10:12:19 +0300

    Number guesser script  with attempts bounds

--- Learn/number_guesser/guesser.py
+++ Learn/number_guesser/guesser.py
@@ -3,19 +3,24 @@ from random import randint
 class Guesser:
     """Class Guesser guesses the number defined by user"""
 
-    def __init__(self, user_number):
+    def __init__(self, user_number, max_attempts):
         self.user_number = user_number
+        self.max_attempts = max_attempts
 
     def set_user_number(self, user_number: "int number that is to guess"):
         self.user_number = user_number
 
     def guess_user_number(self):
-        count = 1
-        self.guessed_number = randint(1, 10)
-        while(self.guessed_number != self.user_number):
-            self.guessed_number = randint(1, 10)
+        count = 0
+        while (count > self.max_attempts) or (count == 0):
+            count = 0
             count += 1
-        self.attempts = count
+            guessed_number = randint(1, 100)
+            while (guessed_number != self.user_number and count <= self.max_attempts):
+                guessed_number = randint(1, 100)
+                count += 1
+            self.attempts = count
+            self.guessed_number = guessed_number
 
     def get_guessed_number(self) -> int:
         return self.guessed_number
@@ -24,9 +29,10 @@ class Guesser:
         return self.attempts
 
 
-number = int(input("Select number in [1..10] range: "))
+number = int(input("Select number in [1..100] range: "))
+attempts = int(input("Select max number of attempts: "))
 
-guesser = Guesser(number)
+guesser = Guesser(number, attempts)
 guesser.guess_user_number()
 print("Guessed number: ", guesser.get_guessed_number())
 print("Attempts: ", guesser.get_attempts())
